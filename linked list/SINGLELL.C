c1ecdf52ef6d04777d4383a8c90c524fc8e22fa3
omkar gavhane <41783462+omkargavhane@users.noreply.github.com>
2018-10-27 20:56:47 +0530

    singly linked list

---
+++ linked list/SINGLELL.C
@@ -0,0 +1,191 @@
+//SE-A_IT_12_15/11/17.             SINGLE_LL
+//OMKAR GAVHANE.
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+typedef struct node
+{
+int data;
+struct node *link;
+}node;
+node *start,*tmp;
+void create_ll();
+void insert_begin();
+void insert_end();
+void insert_before();
+void insert_after();
+void del_begin();
+void del_end();
+void del_before();
+void del_after();
+void del();
+void disp();
+void main()
+{
+int ch;
+clrscr();
+while(ch<12)
+{
+printf("\n***SINGLE LL***");
+printf("\n1.create_ll 2.insert at begin");
+printf("\n3.insert at end 4.insert before 5.insert after");
+printf("\n6.delete begin 7.delete end\n8.delete before \n9.delete after");
+printf("\n10.delete specific node 11.display");
+printf("\nenter choice=");
+scanf("%d",&ch);
+switch(ch)
+{
+case 1:create_ll();
+       break;
+case 2:insert_begin();
+       break;
+case 3:insert_end();
+       break;
+case 4:insert_before();
+       break;
+case 5:insert_after();
+       break;
+case 6:del_begin();
+       break;
+case 7:del_end();
+       break;
+case 8:del_before();
+       break;
+case 9:del_after();
+       break;
+case 10:del();
+	break;
+case 11:disp();
+	break;
+}
+}
+getch();
+}
+void create_ll()
+{
+tmp=(node*)malloc(sizeof(node));
+printf("enter data=");
+scanf("%d",&tmp->data);
+tmp->link=NULL;
+if(start==NULL)
+start=tmp;
+}
+void insert_begin()
+{
+create_ll();
+tmp->link=start;
+start=tmp;
+}
+void insert_end()
+{
+node *t=start;
+create_ll();
+while(t->link!=NULL)
+t=t->link;
+t->link=tmp;
+}
+void insert_before()
+{
+int da;
+node *pre,*t=start;
+create_ll();
+printf("enter the node before that node has to be inserted=");
+scanf("%d",&da);
+while(t->data!=da)
+{
+ pre=t;
+ t=t->link;
+}
+tmp->link=t;
+pre->link=tmp;
+}
+void insert_after()
+{
+int da;
+node *post,*t=start;
+create_ll();
+printf("enter the node after that node has to be inserted=");
+scanf("%d",&da);
+while(t->data!=da)
+ t=t->link;
+post=t->link;
+tmp->link=post;
+t->link=tmp;
+}
+void del_begin()
+{
+node *t=start;
+if(t==NULL) printf("empty list");
+printf("%d deleted",t->data);
+start=start->link;
+free(t);
+}
+void del_end()
+{
+node *pre,*t=start;
+if(t==NULL) printf("empty list");
+while(t->link!=NULL)
+{
+ pre=t;
+ t=t->link;
+}
+printf("%d deleted",t->data);
+pre->link=NULL;
+free(t);
+}
+void del_before()
+{
+int da;
+node *pre,*t=start;
+if(t==NULL) printf("empty list");
+printf("enter node before which node has to be deleted=");
+scanf("%d",&da);
+while(t->link->data!=da)
+{
+pre=t;
+t=t->link;
+}
+printf("%d deleted",t->data);
+pre->link=t->link;
+free(t);
+}
+void del_after()
+{
+node *pre=start,*t;
+int da;
+if(t==NULL) printf("empty list");
+printf("enter node after that node is to be deleted=");
+scanf("%d",&da);
+while(pre->data!=da)
+ pre=pre->link;
+t=pre->link;
+printf("%d deleted",t->data);
+pre->link=t->link;
+free(t);
+}
+void del()
+{
+node *pre,*t=start;
+int da;
+if(t==NULL) printf("empty list");
+printf("enter node to be deleted=");
+scanf("%d",&da);
+while(t->data!=da)
+{
+ pre=t;
+ t=t->link;
+}
+printf("%d deleted",t->data);
+pre->link=t->link;
+free(t);
+}
+void disp()
+{
+node *t=start;
+if(t==NULL) printf("empty list");
+while(t)
+{
+printf("%d_",t->data);
+t=t->link;
+}
+}
